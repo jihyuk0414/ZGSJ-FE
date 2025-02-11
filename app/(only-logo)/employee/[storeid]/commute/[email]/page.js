c9f4343e4dc0ba458a004bceb2fc9046fa786322
jihyuk0414 <dealon2580@naver.com>
2025-02-12 00:01:46 +0900

    update : 문구 변경

--- app/(only-logo)/employee/[storeid]/commute/[email]/page.js
+++ app/(only-logo)/employee/[storeid]/commute/[email]/page.js
@@ -223,6 +223,7 @@ export default function AttendancePage() {
                             <svg className="w-10 h-10 text-green-500 mr-3 animate-bounce" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                             </svg>
+                            <span className="font-medium text-green-700 text-lg"> 핸드폰 화면에선 더 정확한 위치 확인이 가능합니다 →</span>
                         </div>
                         <svg className="w-6 h-6 text-green-600 animate-pulse" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
